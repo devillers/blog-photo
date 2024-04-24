114ce7dc34b0df942688f3879ee517a38fb25ae3
devillers <davidevillers@gmail.com>
2024-04-24 21:16:19 +0200

    popoopo

--- components/Modal.js
+++ components/Modal.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Modal = ({ isOpen, onClose, imageSrc }) => {
   if (!isOpen) return null;
 
+  const handleClickBackdrop = (e) => {
+    e.stopPropagation(); // Empêche la propagation à d'autres éléments
+    onClose(); // Appelle la fonction onClose pour fermer le modal
+  };
+
   return (
     <div
       className="fixed inset-0 bg-slate-600 bg-opacity-10 flex justify-center items-center p-4 z-50"
-      onClick={onClose}
+      onClick={handleClickBackdrop} // Gère le clic sur l'arrière-plan pour fermer le modal
     >
-      <div className="bg-white p-4 rounded-lg max-w-xl max-h-full overflow-auto">
-        <img
+      <div
+        className="bg-white p-4 rounded-lg max-w-xl max-h-full overflow-auto"
+        onClick={(e) => e.stopPropagation()} // Empêche le clic de se propager au div de fond
+      >
+        <Image
           src={imageSrc}
           alt="Popup Image"
-          onClick={(e) => e.stopPropagation()}
-          className="w-auto max-h-screen"
+          className="w-auto max-h-screen p-10 "
+          layout="fill"
+          objectFit="contain"
+          onClick={(e) => e.stopPropagation()} // Empêche également la propagation ici
         />
       </div>
     </div>
